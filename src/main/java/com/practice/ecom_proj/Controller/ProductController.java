a77fd798046febfdbf1008c53fc3963ca4d826c4
AyushShukla26 <ayush.shukla.26@outlook.com>
2026-01-14 09:56:33 +0530

    Added Search feature

--- src/main/java/com/practice/ecom_proj/Controller/ProductController.java
+++ src/main/java/com/practice/ecom_proj/Controller/ProductController.java
@@ -66,5 +66,10 @@ public class ProductController {
                 .body(imageFile);
     }
 
+    @GetMapping({"/products/search"})
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword) {
+        List<Product> products = this.service.searchProducts(keyword);
+        return new ResponseEntity(products, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/practice/ecom_proj/Services/ProductServices.java
+++ src/main/java/com/practice/ecom_proj/Services/ProductServices.java
@@ -39,5 +39,9 @@ public class ProductServices {
 
         return repo.save(product);
     }
+    public List<Product> searchProducts(String keyword) {
+        return this.repo.searchProducts(keyword);
+    }
+
 }
 
